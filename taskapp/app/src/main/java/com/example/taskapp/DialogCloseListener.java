02482832ebd11a7d94baf6de52ca17df0bc8657e
maharaja16 <m.maharaja60@gmail.com>
2021-05-17 19:10:16 -0700

    task-app

---
+++ taskapp/app/src/main/java/com/example/taskapp/DialogCloseListener.java
@@ -0,0 +1,10 @@
+package com.example.taskapp;
+
+import android.content.DialogInterface;
+
+public class DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog) {
+
+    }
+}
+
